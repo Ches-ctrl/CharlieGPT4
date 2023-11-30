61a7878ba360c3d2f42fa8248043fb0d13bfd89b
Charlie Cheesman <djnr.cheesman@gmail.com>
2023-11-30 07:48:04 +0000

    adding config files

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,11 @@
-# $redis = Redis.new(url: ENV["STACKHERO_REDIS_URL_TLS"])
+url = ENV["REDISCLOUD_URL"]
+
+if url
+  Sidekiq.configure_server do |config|
+    config.redis = { url: url }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: url }
+  end
+end
