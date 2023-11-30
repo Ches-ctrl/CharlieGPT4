0a76fce2cf46bb82ad1085c94f9b0f8e16bc02d8
Charlie Cheesman <djnr.cheesman@gmail.com>
2023-11-30 19:44:46 +0000

    updating redis configuration

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,13 @@
-url = ENV["REDISCLOUD_URL"]
+$redis = Redis.new(url: ENV["STACKHERO_REDIS_URL_TLS"])
 
-if url
-  Sidekiq.configure_server do |config|
-    config.redis = { url: url }
-  end
+# url = ENV["REDISCLOUD_URL"]
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: url }
-  end
-end
+# if url
+#   Sidekiq.configure_server do |config|
+#     config.redis = { url: url }
+#   end
+
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: url }
+#   end
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 puts "Creating a user..."
 
-User.create(email: ENV['CHARLIE_EMAIL'], password: ENV['CHARLIE_PASSWORD'], first_name: "Charlie", last_name: "Cheesman")
-puts "Created user:  #{User.first.first_name}"
+User.create(email: ENV['CHARLIE_EMAIL'], password: ENV['CHARLIE_PASSWORD'])
+puts "Created user:  #{User.first}"
 
 # , admin: true
 # TODO: Add admin user
